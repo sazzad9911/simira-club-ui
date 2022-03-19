07f3de810fbf5d59c68346574412be1f730dc46f
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-03-19 21:20:23 +0000

    top brands

--- src/Components/TopBrands.js
+++ src/Components/TopBrands.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import '../Components/css/topbrands.css';
 import Brands from '../Content/Brands';
+import ArrowCircleLeftSharpIcon from '@mui/icons-material/ArrowCircleLeftSharp';
+import ArrowCircleRightSharpIcon from '@mui/icons-material/ArrowCircleRightSharp';
+import Button from '@mui/material/Button';
 
 const TopBrands = (props) => {
     return (
-        <div className='box'> 
+        <div className='box'>
+            <view className='arrowLeft1'>
+                <Button >
+                    <ArrowCircleLeftSharpIcon color="disabled" fontSize='large'
+                    ></ArrowCircleLeftSharpIcon>
+                </Button>
+            </view>
+
+            <view className='arrowRight1'>
+                <Button>
+                    <ArrowCircleRightSharpIcon
+                        fontSize='large'
+                        color="disabled"></ArrowCircleRightSharpIcon>
+                </Button>
+            </view>
             <h2 >Top brands for you</h2>
             <div className='brandShow'>
                 <Brands img='https://lh3.googleusercontent.com/V3UUzurrfYRckyv8JQ6EqhB972GXgmFOCEJkDF884o_cOITGWAfPWqemkNIY8Wp3d7Y'></Brands>
