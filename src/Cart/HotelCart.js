64e44991683a89650dcbceca4fc831e4086732da
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-20 00:39:08 +0600

    ok

--- src/Cart/HotelCart.js
+++ src/Cart/HotelCart.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './css/hotelcart.css';
+import AcUnitIcon from '@mui/icons-material/AcUnit';
 
 const HotelCart = () => {
     return (
-        <div>
-            <div className='hotelcartBody'>
-            <img className='hotelcartImg' src="https://www.berjayahotel.com/sites/default/files/BMH%20PremierRoom%20800x800.jpg"/>
+        <div className='hotelcartBody'>
+            <img className='hotelcartImg' src="https://www.berjayahotel.com/sites/default/files/BMH%20PremierRoom%20800x800.jpg" />
+            <div className='hotelcartBodydiv'>
+                <h2 className='hotelcartText'>Shradha Saburi Palace</h2>
+                <p className='hotelcartText'>Shirdi,Maharashtra</p>
+                <div className='hotelcartIconDiv'>
+                    <AcUnitIcon className='hotelcartIcon'/>
+                    <p className='hotelcartText'>Free for Members</p>
+                </div>
             </div>
         </div>
     );
