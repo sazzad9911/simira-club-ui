c70a87a56f1f8fe9ca3d4cc229c558c16d4644c9
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-19 22:15:59 +0600

    ok

--- src/Cart/HotelCart.js
+++ src/Cart/HotelCart.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import './css/hotelcart.css';
 
 const HotelCart = () => {
     return (
         <div>
+            <div></div>
             <h1>akash</h1>
         </div>
     );
