4974c6bc0396ede8015a474bca8529e89d239af8
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-03-22 22:25:26 +0000

    book show done

--- src/Cart/BookShow.js
+++ src/Cart/BookShow.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import Link from '@mui/material/Link';
 import '../Cart/css/bookshow.css'
 
 const BookShow = () => {
     return (
-        <div className='bookshowbody'>
+
+        <Link className='bookshowbody' href="#">
             <img className="bookshow" src="https://scontent.fdac2-1.fna.fbcdn.net/v/t1.15752-9/275350174_751307125786834_6065488943465361346_n.png?_nc_cat=103&ccb=1-5&_nc_sid=ae9488&_nc_eui2=AeH_FZ_Sfi5Fo1tbyKofGTkkO08Q8V7jTJM7TxDxXuNMk4TFn68iu5PYdVSMMQxFN1QUE59Mm6CvMwSRo9iB-QWz&_nc_ohc=nMW7fLAFFnEAX_Tqh9o&_nc_ht=scontent.fdac2-1.fna&oh=03_AVKm6Zf1NMgrK8KBlM6eI6xudaykrAwuHBOYzyFCcelX-w&oe=62613B85" alt="img1" />
-        </div>
+
+        </Link>
     );
 };
 
