135490f0417d7b595b62a3ea1ccdbef8133a41bd
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-23 00:25:57 +0600

    ok

--- src/Cart/AppOverView.js
+++ src/Cart/AppOverView.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import './css/AppOverView.css';
 
 const AppOverView = () => {
     return (
-        <div>
-            
+        <div className='AppOverViewBody'>
+            <div className='AppOverViewLaftDiv'>
+
+            </div>
+            <div className='AppOverViewRightDiv'>
+                <div>
+                    <text className='AppOverViewBigText'>Get The SmiraClub App</text>
+                    <div>
+                        <p>
+                            We'll send you a link that you can open on your phone to<br></br>
+                            download the app.
+                        </p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
