779811568c8685bad8cf774d84707bfc1494d766
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-23 09:22:18 +0600

    60% done

--- src/Screens/MyProfile.js
+++ src/Screens/MyProfile.js
@@ -5,6 +5,9 @@ import './css/MyProfile.css'
 import PermIdentityIcon from '@mui/icons-material/PermIdentity';
 import LocalPhoneIcon from '@mui/icons-material/LocalPhone';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
+import FemaleIcon from '@mui/icons-material/Female';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
 
 
 const MyProfile = (props) => {
@@ -13,7 +16,7 @@ const MyProfile = (props) => {
         <div className='MyProfileBody'>
             <Header />
             <div className='MyProfileDiv'>
-                <div>
+                <div className='MyProfileText'>
                     <h2>MyProfile</h2>
                 </div>
                 <div className='MyProfileBox'>
@@ -48,32 +51,36 @@ const MyProfile = (props) => {
 
                 <div className='MyProfileBox'>
                     <div className='MyProfileBoxIcon'>
-                        <LocalPhoneIcon className='MyProfileIcon'/>
+                        <FemaleIcon className='MyProfileIcon'/>
 
                     </div>
                     <div className='MyProfileInputBox'>
-                        <input className='MyProfileInput' type='number' placeholder='Phone' />
+                        <input className='MyProfileInput' type='text' placeholder='Gender' />
                     </div>
                 </div>
 
                 <div className='MyProfileBox'>
                     <div className='MyProfileBoxIcon'>
-                        <LocalPhoneIcon className='MyProfileIcon'/>
+                        <CalendarMonthIcon className='MyProfileIcon'/>
 
                     </div>
                     <div className='MyProfileInputBox'>
-                        <input className='MyProfileInput' type='number' placeholder='Phone' />
+                        <input className='MyProfileInput' type='number' placeholder='Calendar' />
                     </div>
                 </div>
 
                 <div className='MyProfileBox'>
                     <div className='MyProfileBoxIcon'>
-                        <LocalPhoneIcon className='MyProfileIcon'/>
+                        <LocationOnIcon className='MyProfileIcon'/>
 
                     </div>
                     <div className='MyProfileInputBox'>
-                        <input className='MyProfileInput' type='number' placeholder='Phone' />
+                        <input className='MyProfileInput' type='text' placeholder='Location' />
                     </div>
+                </div>
+                <div className='MyProfileText'>
+                    <h5>Forgot password?</h5>
+                    <h6>Change password</h6>
                 </div>
             </div>
             <Footer />
