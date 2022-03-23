402b385fe23b30d2b792ea32efaf821adc0f0396
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-03-23 01:02:35 +0000

    log in test

---
+++ src/Components/LogIn.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Link from '@mui/material/Link';
+import '../Components/css/login.css'
+import Button from '@mui/material/Button';
+import GoogleIcon from '@mui/icons-material/Google';
+import Header from './Header';
+
+
+const Login = (props) => {
+
+    return (
+       <div>
+           <Header/>
+            <div className='loginbody'>
+            
+            <div className='loginbodyleft'>
+                <h1>IMAGE</h1>
+            </div>
+            <div className='loginbodyright'>
+                <div className='loginbodyrightbody'>
+                    <h1>Login into your existing <br></br> account</h1>
+                    <div className='textinputview'>
+                        <input className='textinput' type='email' placeholder='Email' />
+
+                    </div>
+                    <div className='textinputview'>
+                        <input className='textinput' type='password' placeholder='Password' />
+                    </div>
+                    <div className='forgotPlink'>
+                        <Link underline="none" color="black" href="#">Forgot Password?</Link>
+                    </div>
+                    <Button >
+                        <div className='loginButton'>
+                            <p className='submitText'>SUBMIT</p>
+                        </div>
+                    </Button>
+                    <div className='underline'>
+
+                    </div>
+                    <Button >
+                    <div className='googleButton'>
+                        <GoogleIcon></GoogleIcon>
+                        <p className='googleText'>Continue with Google</p>
+                    </div>
+                    </Button>
+                    <p>Already have a account? <Link underline="none" color="red" href="#">Login</Link></p>
+                </div>
+            </div>
+        </div>
+       </div>
+    );
+};
+
+export default Login;
