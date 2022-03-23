61d165daedcabe565d4eb44863416126f60700f2
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-03-23 02:19:06 +0000

    sign up done

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ import {
 import Home from './Screens/Home';
 import MyProfile from './Screens/MyProfile';
 import Login from "./Components/LogIn";
+import SignUp from "./Screens/SignUp";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/" element={<Home/>} />
         <Route path="/Profile" element={<MyProfile/>} />
         <Route path="/Login" element={<Login/>}/>
+        <Route path="/SignUp" element={<SignUp/>}/>
       </Routes>
     </Router>
   );
---
+++ src/Screens/SignUp.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
+import Link from '@mui/material/Link';
+import Button from '@mui/material/Button';
+import GoogleIcon from '@mui/icons-material/Google';
+import '../Screens/css/signup.css'
+
+const SignUp = () => {
+    return (
+        <div>
+            <Header />
+            <div className='loginbody1'>
+
+                <div className='loginbodyleft1'>
+                    <img src="https://theupay.com/bank/Assets/login.jpg" height={600} width={400}></img>
+                </div>
+                <div className='loginbodyright1'>
+                    <div className='loginbodyrightbody1'>
+                        <h1>Create account and become <br/>a membe</h1>
+                        <div className='textinputview1'>
+                            <input className='textinput1' type='text' placeholder='Full Name' />
+
+                        </div>
+                        <div className='textinputview1'>
+                            <input className='textinput1' type='email' placeholder='Email' />
+
+                        </div>
+                        <div className='textinputview1'>
+                            <input className='textinput1' type='password' placeholder='Password' />
+                        </div>
+                        <div className='forgotPlink1'>
+                            <Link underline="none" color="black" href="#">Forgot Password?</Link>
+                        </div>
+                        <Button >
+                            <div className='loginButton1'>
+                                <p className='submitText1'>SUBMIT</p>
+                            </div>
+                        </Button>
+                        <div className='underline1'>
+
+                        </div>
+                        <Button >
+                            <div className='googleButton1'>
+                                <GoogleIcon></GoogleIcon>
+                                <p className='googleText1'>Continue with Google</p>
+                            </div>
+                        </Button>
+                        <p>Don’t have a account? <Link underline="none" color="red" href="#">Signup</Link></p>
+                    </div>
+                </div>
+            </div>
+            <Footer />
+        </div>
+    );
+};
+
+export default SignUp;
