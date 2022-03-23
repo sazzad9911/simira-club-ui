e7ed04dc083bfbf5956eb2899b7755f640e4c43c
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-24 05:03:23 +0600

    50% Done

--- src/Screens/Search.js
+++ src/Screens/Search.js
@@ -4,16 +4,20 @@ import './css/Search.css'
 function Search(props) {
     return (
         <div className='ScarchBody'>
-            <img className='ScarchImg' src={'https://thumbs.dreamstime.com/b/pretty-pink-background-soft-burgundy-mauve-rose-colors-vintage-texture-sweet-valentines-day-girls-baby-shower-104265928.jpg'} />
+            <img className='ScarchImg' 
+            src={'https://thumbs.dreamstime.com/b/pretty-pink-background-soft-burgundy-mauve-rose-colors-vintage-texture-sweet-valentines-day-girls-baby-shower-104265928.jpg'} />
             <div className='ScarchImgDiv'>
-                <h1>Over 45 hotel & 100+ deals across 10 cities</h1>
-                <div className='MyProfileBox'>
-                    <div className='MyProfileInputBox'>
-                        <select className='MyProfileSelect'>
+                <h1 className='ScarchImgText'>Over 45 hotel & 100+ deals across 10 cities</h1>
+                <div className='ScarchBox'>
+                    <div className='ScarchInputBox'>
+                        <input className='ScarchInput' type='text'
+                         placeholder='Search by hotel,deal,restaurant' />
+                        <select className='ScarchSelect'>
                             <option value="opel">Hotel</option>
                             <option value="audi">Deals</option>
                         </select>
-                        <button>Search</button>
+                        <button className='ScarchButton'>Search</button>
+                        
                     </div>
                 </div>
             </div>
