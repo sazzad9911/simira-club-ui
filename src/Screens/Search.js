c9c117ce90914bb8649382c32a8b675a63cafda6
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-24 20:41:01 +0600

    50% Done

--- src/Screens/Search.js
+++ src/Screens/Search.js
@@ -1,25 +1,31 @@
 import React from 'react';
 import './css/Search.css'
+import Img from "./css/Search.PNG"
 
 function Search(props) {
     return (
         <div className='ScarchBody'>
-            <img className='ScarchImg' 
-            src={'https://thumbs.dreamstime.com/b/pretty-pink-background-soft-burgundy-mauve-rose-colors-vintage-texture-sweet-valentines-day-girls-baby-shower-104265928.jpg'} />
+            <img src={Img} className='ScarchImg'/>
+
             <div className='ScarchImgDiv'>
-                <h1 className='ScarchImgText'>Over 45 hotel & 100+ deals across 10 cities</h1>
+                <div className='ScarchImgDivText'>
+                    <h1 className='ScarchImgText'>Over 45 hotel & 100+ deals across 10 cities</h1>
+
+                </div>
                 <div className='ScarchBox'>
                     <div className='ScarchInputBox'>
                         <input className='ScarchInput' type='text'
-                         placeholder='Search by hotel,deal,restaurant' />
+                            placeholder='Search by hotel,deal,restaurant' />
                         <select className='ScarchSelect'>
                             <option value="opel">Hotel</option>
                             <option value="audi">Deals</option>
                         </select>
                         <button className='ScarchButton'>Search</button>
-                        
+
                     </div>
                 </div>
+            </div>
+            <div>
             </div>
         </div>
     );
