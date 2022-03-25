3b6bab654ad4705004a3ff6477a0c6a10363b0f0
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-26 01:39:41 +0600

    ok

--- src/Screens/Careers.js
+++ src/Screens/Careers.js
@@ -18,6 +18,11 @@ function Career() {
             button2='Remote'
             button3='Sales'
             ></Cart>
+            <Cart text='Sale Associate'
+            button1='Permanent'
+            button2='Remote'
+            button3='Sales'
+            ></Cart>
         </div>
     );
 }
