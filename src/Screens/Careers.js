f268e2d10d1210941800ae36976c41ac606321a2
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-03-26 01:14:18 +0600

    done

--- src/Screens/Careers.js
+++ src/Screens/Careers.js
@@ -1,6 +1,8 @@
 import React from "react";
 import './css/Careers.css'
 import Img from './Image/Careers.PNG'
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import Link from '@mui/material/Link';
 
 function Career() {
     return (
@@ -11,23 +13,33 @@ function Career() {
                     <h1 className="CareerImgText">Careers</h1>
                 </div>
             </div>
-            <div className="CareerBox">
-                <div className="CareerBoxDiv">
-                    <div>
-                        <div>
-                            <h2>Sale Associate</h2>
-                            <button>Permanent</button>
-                            <button>Remote</button>
-                            <button>Sales</button>
-                        </div>
-                        <div>
+            <Cart text='Sale Associate'
+            button1='Permanent'
+            button2='Remote'
+            button3='Sales'
+            ></Cart>
+        </div>
+    );
+}
 
-                        </div>
+export default Career;
+
+function Cart(props) {
+    return (
+        <div className="CareerBox">
+            <div className="CareerBoxMainButton">
+                <div className="CareerBoxDiv">
+                    <div className="CareerBoxRightDiv">
+                        <h2>{props.text}</h2>
+                        <button className="CareerBoxFirstButton">{props.button1}</button>
+                        <button className="CareerBoxScendButton">{props.button2}</button>
+                        <button className="CareerBoxThridButton">{props.button3}</button>
+                    </div>
+                    <div className="CareerBoxLeftDiv">
+                    <Link href="/"><ArrowForwardIosIcon /></Link>
                     </div>
                 </div>
             </div>
         </div>
     );
 }
-
-export default Career;
