95cdc557f492fb09c55b1e8ae3c3272178af36e5
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-04-04 00:11:33 +0600

    10% done

--- src/Cart/FilterCart.js
+++ src/Cart/FilterCart.js
@@ -4,9 +4,22 @@ import '../Cart/css/FilterCart.css'
 const FilterCart = (props) => {
     return (
         <div className='FilterCartBody'>
-            <div style={{display:'flex'}}>
+            <div style={{ display: 'flex' }}>
                 <h2>Filters</h2>
                 <button className='FilterCartButton'>Clear All</button>
+            </div>
+            <div className='FilterCartDivLine' />
+            <p>Sort by</p>
+            <div style={{display:'grid'}}>
+                <button>Popularity</button>
+                <button>Ratings</button>
+                <button>Amenities</button>
+            </div>
+            <div />
+            <div className='FilterCartDivLine' />
+            <p>Ratings</p>
+            <div style={{ display: 'flex' }}>
+
             </div>
         </div>
     );
