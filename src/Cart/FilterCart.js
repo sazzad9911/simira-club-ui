7ecdb604415aa460bba613d81365cbeff2b13e2f
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-04-03 23:46:47 +0600

    FilterCard 5% done

--- src/Cart/FilterCart.js
+++ src/Cart/FilterCart.js
@@ -3,8 +3,11 @@ import '../Cart/css/FilterCart.css'
 
 const FilterCart = (props) => {
     return (
-        <div>
-            <p>hi</p>
+        <div className='FilterCartBody'>
+            <div style={{display:'flex'}}>
+                <h2>Filters</h2>
+                <button className='FilterCartButton'>Clear All</button>
+            </div>
         </div>
     );
 };
