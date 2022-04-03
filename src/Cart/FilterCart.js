440cc6c7f42ca41888d724cb4db4c8419fad4cd6
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-04-04 01:29:08 +0600

    FilterCart done

--- src/Cart/FilterCart.js
+++ src/Cart/FilterCart.js
@@ -10,40 +10,68 @@ const FilterCart = (props) => {
                 <button className='FilterCartButton'>Clear All</button>
             </div>
             <div className='FilterCartDivLine' />
-            <p>Sort by</p>
-            <div style={{ display: 'grid' }}>
-                <button>Popularity</button>
-                <button>Ratings</button>
-                <button>Amenities</button>
+            <p style={{ marginLeft: '15px', marginTop: '30px' }}>Sort by</p>
+            <div style={{ display: 'grid', marginTop: '20px' }}>
+                <button className='FilterCartButtonPopu'>Popularity</button>
+                <button className='FilterCartButtonRat'>Ratings</button>
+                <button className='FilterCartButtonAme'>Amenities</button>
             </div>
-            <div />
+            <div style={{ marginTop: '30px' }} />
             <div className='FilterCartDivLine' />
-            <p>Ratings</p>
-            <div style={{ display: 'flex' }}>
+            <p style={{ marginTop: '30px', marginLeft: '15px' }}>Ratings</p>
+            <div style={{
+                display: 'flex',
+                marginLeft: '15px',
+                marginTop: '10px',
+            }}>
                 <StarIcon style={{ color: '#64B657' }} />
                 <StarIcon style={{ color: '#64B657' }} />
                 <StarIcon style={{ color: '#64B657' }} />
                 <StarIcon style={{ color: '#64B657' }} />
                 <StarIcon style={{ color: '#64B657' }} />
             </div>
-            <div style={{ display: 'flex', marginTop: '10px' }}>
+            <div style={{
+                display: 'flex',
+                marginTop: '10px',
+                marginLeft: '15px'
+            }}>
                 <StarIcon style={{ color: '#B2DBAC' }} />
                 <StarIcon style={{ color: '#B2DBAC' }} />
                 <StarIcon style={{ color: '#B2DBAC' }} />
                 <StarIcon style={{ color: '#B2DBAC' }} />
             </div>
-            <div style={{ display: 'flex', marginTop: '10px' }}>
+            <div style={{
+                display: 'flex',
+                marginTop: '10px',
+                marginLeft: '15px'
+            }}>
                 <StarIcon style={{ color: '#D7FB73' }} />
                 <StarIcon style={{ color: '#D7FB73' }} />
                 <StarIcon style={{ color: '#D7FB73' }} />
             </div>
-            <div style={{ display: 'flex', marginTop: '10px' }}>
+            <div style={{
+                display: 'flex',
+                marginTop: '10px',
+                marginLeft: '15px'
+            }}>
                 <StarIcon style={{ color: '#FBDD73' }} />
                 <StarIcon style={{ color: '#FBDD73' }} />
             </div>
-            <div style={{ display: 'flex', marginTop: '10px' }}>
+            <div style={{
+                display: 'flex',
+                marginTop: '10px',
+                marginLeft: '15px'
+            }}>
                 <StarIcon style={{ color: '#E47768' }} />
             </div>
+            <div style={{ marginTop: '30px' }} />
+            <div className='FilterCartDivLine' />
+            <p style={{ marginTop: '30px', marginLeft: '15px' }}>Categories</p>
+            <div style={{ display: 'flex' }}>
+                <button className='FilterCartButtonDel'>Deluxe</button>
+                <button className='FilterCartButtonVil'>Villas</button>
+            </div>
+            <button className='FilterCartButtonFarm'>Farm House</button>
         </div>
     );
 };
