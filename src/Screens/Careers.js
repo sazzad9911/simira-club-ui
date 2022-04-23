8308c365caab6ccd6c56d0fe49b44dd8fa88ffb5
akashsdk <93345259+akashsdk@users.noreply.github.com>
2022-04-24 03:20:36 +0600

    ok

--- src/Screens/Careers.js
+++ src/Screens/Careers.js
@@ -1,7 +1,6 @@
 import React from "react";
 import './css/Careers.css'
 import Img from './Image/Careers.PNG'
-import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Link from '@mui/material/Link';
 
 function Career() {
@@ -13,11 +12,20 @@ function Career() {
                     <h1 className="CareerImgText">Careers</h1>
                 </div>
             </div>
-            <Cart text='Sale Associate'
-            button1='Permanent'
-            button2='Remote'
-            button3='Sales'
-            ></Cart>
+            <div className="CareerCartDiv">
+                <Cart text='Sale Associate'
+                    button1='Permanent'
+                    button2='Remote'
+                    button3='Sales'
+                ></Cart>
+
+                <Cart text='Sale Associate'
+                    button1='Permanent'
+                    button2='Remote'
+                    button3='Sales'
+                ></Cart>
+                <div className="CareerEndDiv" />
+            </div>
         </div>
     );
 }
@@ -30,13 +38,17 @@ function Cart(props) {
             <div className="CareerBoxMainButton">
                 <div className="CareerBoxDiv">
                     <div className="CareerBoxRightDiv">
-                        <h2>{props.text}</h2>
+                        <p className="CareerBoxText">{props.text}</p>
                         <button className="CareerBoxFirstButton">{props.button1}</button>
                         <button className="CareerBoxScendButton">{props.button2}</button>
                         <button className="CareerBoxThridButton">{props.button3}</button>
                     </div>
                     <div className="CareerBoxLeftDiv">
-                    <Link href="/JobDescription"><ArrowForwardIosIcon /></Link>
+                        <Link href="/JobDescription">
+                            <svg width="25" height="15" viewBox="0 0 9 15" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M0.451377 14.4138C0.183905 14.1693 0.15959 13.7868 0.37843 13.5172L0.451377 13.44L6.94998 7.50009L0.451377 1.5602C0.183905 1.31573 0.15959 0.93319 0.37843 0.663622L0.451377 0.586392C0.718848 0.341928 1.1374 0.319705 1.43234 0.51972L1.51684 0.586392L8.54851 7.01319C8.81598 7.25765 8.8403 7.6402 8.62146 7.90977L8.54851 7.987L1.51684 14.4138C1.22262 14.6827 0.745595 14.6827 0.451377 14.4138Z" fill="black" />
+                            </svg>
+                        </Link>
                     </div>
                 </div>
             </div>
