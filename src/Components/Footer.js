e361c092af5889fc1a61b7115c1897c763d9bab2
amandiu <79790568+amandiu@users.noreply.github.com>
2022-04-24 20:47:35 +0600

    Footer Done

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -20,45 +20,45 @@ const Footer = () => {
             <div className='Footerbodyf'>
                 <div className='footerbox'>
                     <div className='Footertxtt'>
-                        <h5>COMPANY</h5>
+                        <h5 className='Footerfont-style'>COMPANY</h5>
                         <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Careers</Link><br></br>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Careers</Link><br></br>
                         </div>
                         <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Contact</Link><br></br>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Contact</Link><br></br>
                         </div>
                         <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none" >Sitemap</Link>
-                        </div>
-                    </div>
-                    <div className='Footertxt'>
-                        <h4>For You</h4>
-                        <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Membership</Link><br></br>
-                        </div>
-                        <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Privacy</Link><br></br>
-                        </div>
-                        <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Tems</Link><br></br>
-                        </div>
-                        <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Support</Link>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Sitemap</Link>
                         </div>
                     </div>
                     <div className='Footertxt'>
-                        <h5>DISCOVER</h5>
+                        <h4 className='Footerfont-style'>For You</h4>
                         <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Popular Deals</Link><br></br>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Membership</Link><br></br>
                         </div>
                         <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Popular Hotels</Link><br></br>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Privacy</Link><br></br>
                         </div>
                         <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">SmiraClub Member</Link><br></br>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Tems</Link><br></br>
                         </div>
                         <div className='Footerlink'>
-                            <Link href="#" color="inherit" underline="none">Business With Us</Link>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Support</Link>
+                        </div>
+                    </div>
+                    <div className='Footertxt'>
+                        <h5 className='Footerfont-style'>DISCOVER</h5>
+                        <div className='Footerlink'>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Popular Deals</Link><br></br>
+                        </div>
+                        <div className='Footerlink'>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">Popular Hotels</Link><br></br>
+                        </div>
+                        <div className='Footerlink'>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} letterSpacing="0.03em">SmiraClub Member</Link><br></br>
+                        </div>
+                        <div className='Footerlink'>
+                            <Link href="#" color="#808080" underline="none" fontSize={18} fontWeight={400} letterSpacing="0.03em">Business With Us</Link>
                         </div>
                     </div>
                 </div>
@@ -67,10 +67,10 @@ const Footer = () => {
                     <p className='FooterHed'>Get the SmiraClub App</p>
                     <div className='FooterSmiraClub'>
                         <div className='Footerbodytwoone'>
-                            <Link href="/"><img className='Footerimg' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png' /></Link>
+                            <Link href="/"><img className='Footer-img' src='https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png' /></Link>
                         </div>
                         <div className='Footerbodytwoone'>
-                            <Link href="/"><img className='Footerimg' src='https://www.nysora.com/wp-content/uploads/2020/04/apple-store-badge.png' /></Link>
+                            <Link href="/"><img className='Footer-img' src='https://www.nysora.com/wp-content/uploads/2020/04/apple-store-badge.png' /></Link>
                         </div>
                     </div>
                 </div>
