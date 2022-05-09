7ee580279097aad977b071fea953e7de02bd906d
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2022-05-09 20:49:44 +0100

    ok

--- src/Components/TopBrands.js
+++ src/Components/TopBrands.js
@@ -37,8 +37,7 @@ const TopBrands = (props) => {
             console.log(err.message)
         })
     }, [])
-    const icon = ``
-    return (
+    return ( 
         <div className='box'>
             <div className='arrowLeft1'>
                 <Button onClick={Left}>
@@ -48,8 +47,8 @@ const TopBrands = (props) => {
                         </g>
                         <path d="M33.5171 35.294C33.8206 35.0166 33.8482 34.5824 33.5999 34.2764L33.5171 34.1888L26.1411 27.4469L33.5171 20.7051C33.8206 20.4276 33.8482 19.9935 33.5999 19.6875L33.5171 19.5998C33.2135 19.3224 32.7384 19.2971 32.4037 19.5242L32.3078 19.5998L24.3268 26.8943C24.0232 27.1718 23.9956 27.606 24.244 27.9119L24.3268 27.9996L32.3078 35.294C32.6417 35.5993 33.1831 35.5993 33.5171 35.294Z" fill="black" />
                         <defs>
-                            <filter id="filter0_d_181_2301" x="0.0022583" y="0.446777" width="59.9955" height="60" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <filter id="filter0_d_181_2301" x="0.0022583" y="0.446777" width="59.9955" height="60" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
                                 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
                                 <feOffset dy="3" />
                                 <feGaussianBlur stdDeviation="5" />
@@ -60,6 +59,7 @@ const TopBrands = (props) => {
                             </filter>
                         </defs>
                     </svg>
+
                 </Button>
             </div>
 
@@ -71,8 +71,8 @@ const TopBrands = (props) => {
                         </g>
                         <path d="M26.4829 35.6803C26.1793 35.4028 26.1517 34.9686 26.4001 34.6626L26.4829 34.575L33.8588 27.8332L26.4829 21.0913C26.1793 20.8139 26.1517 20.3797 26.4001 20.0737L26.4829 19.9861C26.7865 19.7086 27.2615 19.6834 27.5963 19.9104L27.6922 19.9861L35.6732 27.2805C35.9768 27.558 36.0044 27.9922 35.756 28.2981L35.6732 28.3858L27.6922 35.6803C27.3582 35.9855 26.8168 35.9855 26.4829 35.6803Z" fill="black" />
                         <defs>
-                            <filter id="filter0_d_132_519" x="0.00219727" y="0.833008" width="59.9955" height="60" filterUnits="userSpaceOnUse" colorInterpolationFilters="sRGB">
-                                <feFlood floodOpacity="0" result="BackgroundImageFix" />
+                            <filter id="filter0_d_132_519" x="0.00219727" y="0.833008" width="59.9955" height="60" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                <feFlood flood-opacity="0" result="BackgroundImageFix" />
                                 <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0" result="hardAlpha" />
                                 <feOffset dy="3" />
                                 <feGaussianBlur stdDeviation="5" />
